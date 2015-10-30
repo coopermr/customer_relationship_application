81315b13aa4e745deec2c5eda03f2f04aa4260f7
Mike Cooper <shivagogo@gmail.com>
2015-10-30 16:39:46 -0400

    Add id

--- CRM.rb
+++ CRM.rb
@@ -1,8 +1,13 @@
 class Contact
+	attr_reader :id
+	attr_accessor :first_name, :last_name, :email, :notes
+
 	def initialize(first_name, last_name, email, notes)
 		@first_name = first_name
 		@last_name = last_name
 		@email = email
 		@notes = notes
+		@id = @@id
+		@@id +=1
 	end
 end
